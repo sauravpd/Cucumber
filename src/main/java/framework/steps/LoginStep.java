343ab314cb7a1109c6cf0c824ceceeda142f6ddf
saurav <sauravpd58@gmail.com>
2018-08-24 22:08:44 +0530

    added steps defination

---
+++ src/main/java/framework/steps/LoginStep.java
@@ -0,0 +1,36 @@
+package framework.steps;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class LoginStep 
+{
+	@Given("^I navigate to the login page$")
+	public void iNavigateToTheLoginPage() throws Throwable 
+	{
+		// TODO
+	}
+
+	@And("^I click login button$")
+	public void iClickLoginButton() throws Throwable 
+	{
+	
+		// TODO
+	}
+
+	@And("^I enter the following for Login$")
+	public void iEnterTheFollowingForLogin(DataTable table) throws Throwable 
+	{
+
+		// TODO
+	}
+
+	@Then("^I click on logout$")
+	public void i_click_on_logout() throws Throwable
+	{
+
+		// TODO
+	}
+}
